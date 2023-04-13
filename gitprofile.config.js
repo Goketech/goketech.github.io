9e9eb5cdf3153d36040ada97093fdbe393117d4e
Modupe Akanni <modupe775@gmail.com>
2023-04-13 16:49:40 +0100

    update config

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -3,7 +3,7 @@
 const config = {
   github: {
     username: 'goketech', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
+    sortBy: 'updated', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: true, // Forked projects will not be displayed if set to true.
