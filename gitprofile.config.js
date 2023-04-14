eb5f1e20fd55bde2306cedfdd8fd1483ffbdf521
Modupe Akanni <modupe775@gmail.com>
2023-04-14 07:14:32 +0100

    update config

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -101,7 +101,7 @@ const config = {
   blog: {
     source: 'dev', // medium | dev
     username: 'goketech', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    limit: 1, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
